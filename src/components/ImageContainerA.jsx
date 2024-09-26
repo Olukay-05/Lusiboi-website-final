595253693bcd6703bedd7d1c194b4906f55654ed
Olukay-05 <olukayode.ogunnowo@yahoo.com>
2024-09-26 11:28:56 +0100

    feat: Added borders to image containers

--- src/components/ImageContainerA.jsx
+++ src/components/ImageContainerA.jsx
@@ -10,14 +10,14 @@ function ImageContainerA() {
   return (
     <article className="place-self-center md:place-self-start">
       <div className="relative rounded-xl w-[15rem] h-[15rem] sm:w-[16.5rem] sm:h-64 md:w-[18rem] md:h-72 lg:w-[22rem] lg:h-[22rem] xl:w-[26rem] xl:h-[26rem] md:rounded-none ">
-        <div className=" absolute bottom-0 right-0 z-10">
+        <div className=" absolute bottom-0 right-0 z-10 outline outline-2 outline-yellow-200 rounded-[3rem] lg:rounded-[5rem]">
           <img
             src={movieProductionB}
             alt="image2"
             className="lg:block max-w-[12rem] sm:max-w-48 md:max-w-56 lg:max-w-72 xl:max-w-[22rem] rounded-[3rem] lg:rounded-[5rem]"
           />
         </div>
-        <div className="absolute">
+        <div className="absolute outline outline-2 outline-yellow-200 rounded-[3rem] lg:rounded-[5rem]">
           <img
             src={movieProductionA}
             alt="image"
--- src/components/ImageContainerD.jsx
+++ src/components/ImageContainerD.jsx
@@ -10,14 +10,14 @@ function ImageContainerD() {
   return (
     <article className="grid place-self-center md:place-self-end order-1 md:order-none">
       <div className="relative rounded-xl w-[15rem] h-[15rem] sm:w-[16.5rem] sm:h-64 md:w-[18rem] md:h-72 lg:w-[22rem] lg:h-[22rem] xl:w-[26rem] xl:h-[26rem] md:rounded-none">
-        <div className="absolute bottom-0 right-0 z-10">
+        <div className="absolute bottom-0 right-0 z-10 outline outline-2 outline-yellow-200 rounded-[3rem] lg:rounded-[5rem]">
           <img
             src={eventManagementA}
             alt="image2"
             className="lg:block max-w-[12rem] sm:max-w-48 md:max-w-56 lg:max-w-72 xl:max-w-[22rem] rounded-3xl"
           />
         </div>
-        <div className="absolute">
+        <div className="absolute outline outline-2 outline-yellow-200 rounded-[3rem] lg:rounded-[5rem]">
           <img
             src={eventManagementB}
             alt="image"
